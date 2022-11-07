29cbdb028c3385948b94862afe374cceab054d63
williamantoline <williamantoline2004@gmail.com>
2022-11-07 12:14:33 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.get('/test', (req, res) => {
+    res.send({message: 'ok'});
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
