996b2e5bd63b6f25d8a9a79f446a3158ca0658fd
Angeline Ho <angelineho16.03@gmail.com>
2022-11-08 11:43:16 +0700

    method get movie:id & error handling

--- server.js
+++ server.js
@@ -13,6 +13,28 @@ app.get('/movies', (req, res) => {
   res.send(movie)
 })
 
+app.get('/movie/:id', (req, res) => {
+  const movie = loadMovie()
+  let foundMovie = null
+  const keys = Object.keys(movie)
+  for(let i=0; i<keys.length; i++){
+    if (movie[keys[i]].id == req.params.id) {
+      foundMovie = movie[keys[i]];
+      break;
+    }
+  }
+  if(foundMovie == null){
+    res.status(404).send("Movie Not Found")
+  }else{
+    res.send(foundMovie)
+    res.status(200)
+  }
+})
+
+app.use('/', (req,res) => {
+  res.status(404).send("File Not Found")
+})
+
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
