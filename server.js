c8c2cad895788ba3da0c91f6a59181185d1a04c9
williamantoline <williamantoline2004@gmail.com>
2022-11-12 13:05:13 +0700

    add host name to console log

--- server.js
+++ server.js
@@ -1,52 +1,59 @@
-const express = require('express')
-const cors = require('cors')
-const { loadMovie, findMovie, addMovie, deleteMovie, updateMovie } = require('./movie')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const {
+  loadMovie,
+  findMovie,
+  addMovie,
+  deleteMovie,
+  updateMovie,
+} = require("./movie");
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const PORT = 3000
+const PORT = 3000;
+const HOST = "http://localhost:3000";
 
-app.get('/api/movies', (req, res) => {
-  const movie = loadMovie()
-  res.status(200).send(movie)
-})
+app.get("/api/movies", (req, res) => {
+  const movie = loadMovie();
+  res.status(200).send(movie);
+});
 
-app.get('/api/movie/:id', (req, res) => {
-  let found = findMovie(req.params.id)
-  if(found == null){
-    res.status(404).send("Movie is not found")
-  }else{
-    res.send(found).status(200)
+app.get("/api/movie/:id", (req, res) => {
+  let found = findMovie(req.params.id);
+  if (found == null) {
+    res.status(404).send("Movie is not found");
+  } else {
+    res.send(found).status(200);
   }
-})
+});
 
-app.put('/api/movie/:id', (req, res) => {
-  let found = findMovie(req.params.id)
-  if(found == null){
-    addMovie(req.body)
-    res.status(200).send("Movie added successfully")
-  }else{
-    updateMovie(req.params.id, req.body)
-    res.status(200).send("Movie updated successfully")
+app.put("/api/movie/:id", (req, res) => {
+  let found = findMovie(req.params.id);
+  if (found == null) {
+    addMovie(req.body);
+    res.status(200).send("Movie added successfully");
+  } else {
+    updateMovie(req.params.id, req.body);
+    res.status(200).send("Movie updated successfully");
   }
-})
+});
 
-app.delete('/api/movie/:id', (req, res) => {
-  let found = findMovie(req.params.id)
-  if(found == null){
-    res.status(404).send("Movie is not found")
-  }else{
-    deleteMovie(req.params.id)
-    res.status(200).send("Movie deleted successfully")
+app.delete("/api/movie/:id", (req, res) => {
+  let found = findMovie(req.params.id);
+  if (found == null) {
+    res.status(404).send("Movie is not found");
+  } else {
+    deleteMovie(req.params.id);
+    res.status(200).send("Movie deleted successfully");
   }
-})
+});
 
-app.use('/', (req,res) => {
-  res.status(404).send("File Not Found")
-})
+app.use("/", (req, res) => {
+  res.status(404).send("File Not Found");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on ${HOST}`);
 });
